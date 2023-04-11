db1a50c1fe9e7b80273a108930677df880c39799
Pritesh Yadav <priteshyadav2015@gmail.com>
2023-04-11 18:54:43 +0530

    file for image drive added

---
+++ dashboard/left-sidebar.php
@@ -0,0 +1,42 @@
+<div class="left-sidebar-pro">
+    <nav id="sidebar" class="">
+        <div class="sidebar-header">
+            <a href="index.php"><img class="main-logo" src="img/logo/logo.png" alt="" /></a>
+            <strong><a href="index.php"><img src="img/logo/logosn.png" alt="" /></a></strong>
+        </div>
+        <div class="left-custom-menu-adp-wrap comment-scrollbar">
+            <nav class="sidebar-nav left-sidebar-menu-pro">
+                <ul class="metismenu" id="menu1">
+                    <li>
+                        <a class="has-arrow" href="master.php" aria-expanded="false">
+                            <span class="glyphicon glyphicon-th-list" aria-hidden="true"></span>
+                            <span class="mini-click-non">Master</span></a>
+                        <ul class="submenu-angle" aria-expanded="false">
+                            <li><a title="master List" href="master.php"><span class="mini-sub-pro">User
+                                        Master Lists</span></a></li>
+                            <li><a title="Add Master List" href="add-masterlist.php"><span class="mini-sub-pro">Add
+                                        Master List</span></a></li>
+
+
+                        </ul>
+                    </li>
+
+
+                    <li>
+                        <a class="has-arrow" href="all-courses.php" aria-expanded="false">
+                            <span class="glyphicon glyphicon-user
+                                " aria-hidden="true"></span>
+                            <span class="mini-click-non">Users</span></a>
+                        <ul class="submenu-angle" aria-expanded="false">
+                            <li><a title="User List" href="user.php"><span class="mini-sub-pro">User
+                                        List</span></a></li>
+                            <li><a title="Add User" href="add-user.php"><span class="mini-sub-pro">Add
+                                        User</span></a></li>
+                        </ul>
+                    </li>
+
+                </ul>
+            </nav>
+        </div>
+    </nav>
+</div>
