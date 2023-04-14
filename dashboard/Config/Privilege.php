0c13fcb652814972af75b8cb81eee6ca7a373dc8
Pritesh Yadav <priteshyadav2015@gmail.com>
2023-04-14 19:18:12 +0530

    session and config added for consant variables

---
+++ dashboard/Config/Privilege.php
@@ -0,0 +1,46 @@
+<?php
+class Privilege
+{
+    const VIEW_DASHBOARD = "view_dashboard";
+    
+    const VIEW_DRIVE = "view_drive";
+    const CREATE_DRIVE = "create_drive";
+    const UPDATE_DRIVE = "update_drive";
+
+    const VIEW_MASTER = "view_master";
+    const CREATE_MASTER = "create_master";
+    const UPDATE_MASTER = "update_master";
+
+    const VIEW_USER = "view_user";
+    const CREATE_USER = "create_user";
+    const UPDATE_USER = "update_user";
+
+    const VIEW_PROJECT = "view_project";
+    const CREATE_PROJECT = "create_project";
+    const UPDATE_PROJECT = "update_project";
+
+    const VIEW_TASK_LOG = "view_task_log";
+    
+    const VIEW_TASK = "view_task";
+    const CREATE_TASK = "create_task";
+    const UPDATE_TASK_LOG = "update_task";
+
+    const VIEW_ROLES_PERMISSION = "view_roles_permission";
+    const CREATE_ROLES_PERMISSION = "create_roles_permission";
+    const UPDATE_ROLES_PERMISSION = "update_roles_permission";
+
+    const VIEW_TICKET = "view_ticket";
+    const CREATE_TICKET = "create_ticket";
+    const UPDATE_TICKET = "update_ticket";
+
+    const VIEW_TICKET_MANAGEMENT = "view_ticket_managment";
+    const CREATE_TICKET_MANAGEMENT = "create_ticket_managment";
+    const UPDATE_TICKET_MANAGEMENT = "update_ticket_managment";
+
+    const VIEW_PROFILE = "view_profile";
+    const CREATE_PROFILE = "create_profile";
+    const UPDATE_PROFILE = "update_profile";
+
+    const VIEW_SETTING = "view_setting";
+    const CREATE_SETTING = "create_setting";
+}
---
+++ dashboard/Config/SessionConfig.php
@@ -0,0 +1,7 @@
+<?php
+class SessionConfig
+{
+    const PRIVILAGS = "privilages";
+    const IS_LOGGED_IN = "isLoggedIn";
+    const USER_DETAILS = "userDetails";
+}
