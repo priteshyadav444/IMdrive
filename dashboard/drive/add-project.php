71d724a96b96974101ac760494d2879279f13154
priteshyadav444 <priteshyadav2015@Gmail.com>
2023-04-19 00:43:32 +0530

    add project form added

--- dashboard/drive/add-project.php
+++ dashboard/drive/add-project.php
@@ -1,3 +1,7 @@
+<?php
+@session_start();
+require_once '../shared/check-login.php';
+?>
 <!doctype html>
 <html class="no-js" lang="en">
 
@@ -7,12 +11,20 @@
   <title>Add Project | Image Drive</title>
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <?php require_once '../shared/head-link.php'; ?>
+  <?php
+  require_once '../shared/head-link.php';
+  require_once '../Config/SessionConfig.php';
+  require_once '../Config/Privilege.php';
+  require_once "../library/validation/vendor/autoload.php";
+  require_once "../database/Connection.php";
+  require_once "../database/User.php";
+
+  ?>
 
 </head>
 
 <body>
-  <!--[if lt IE 8]>
+  <!--[if lt IE 8]> 
 		<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
 	<![endif]-->
   <!-- Start Left menu area -->
@@ -23,7 +35,53 @@
   <div class="all-content-wrapper">
     <?php include_once '../shared/header-top.php'; ?>
 
+    <?php
 
+    use Validation\Validators\FileValidator;
+    use Validation\Validators\FormValidator;
+
+    $obj = new FormValidator();
+
+
+    // Generate select options
+    $connection = new Connection();
+    $user = new User($connection->getConnection());
+    $options = " <option value='null'>Select Deliverable Type</option>";
+
+    $deliverables = $user->getAllDelverable();
+    foreach ($deliverables as $deliverable) {
+      $options .= '<option value="' . $deliverable['deliverable_id'] . '">' . $deliverable['deliverable_name'] . '</option>';
+    }
+
+    if (isset($_POST['submit_project_create'])) {
+
+      $extention = pathinfo($_FILES["logo"]['name'], PATHINFO_EXTENSION);
+
+      in_array($extention, array("jpg", "png", "jpeg", "svg"));
+      
+      $validations = [
+        'name' => 'required|string',
+        'deliverable' => 'required',
+        'description' => 'required|string',
+        'logo' => "required|filetype:{$extention}|max:40000",
+      ];
+
+
+      $file = new FileValidator($_FILES, "logo", "files");
+      $file->upload();
+
+      // if all validation Passed
+      if (!$obj->validate($_POST, $validations)->isError()) {
+
+        $connection = new Connection();
+        $user = new User($connection->getConnection());
+
+        // $isFormDataValid = $user->createProject($obj->senitizeInput($_POST['name']));
+      }
+    }
+
+
+    ?>
     <!-- Single pro tab review Start-->
     <div class="single-pro-review-area mt-t-30 mg-b-15 mg-t-20">
       <div class="container-fluid">
@@ -39,14 +97,26 @@
                   <div class="row">
                     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                       <div class="review-content-section">
-                        <form id="add-department" action="#" class="add-department">
+                        <?php
+                        if ($obj->isError() != false && isset($_POST['submit_project_create'])) {
+                          echo '<div class="alert alert-danger" role="alert">';
+                          foreach ($obj->all() as $error)
+                            echo "<li>$error</li>";
+                          echo '</div>';
+                        } else if (isset($isFormDataValid) && $isFormDataValid  && isset($_POST['submit_project_create'])) {
+                          echo '<div class="alert alert-success" role="alert">';
+                          echo "<li>Project Created Successfully </li>";
+                          echo '</div>';
+                        }
+                        ?>
+                        <form action="" class="add-department" method="POST" enctype="multipart/form-data">
                           <div class="row">
                             <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
                               <div class="form-group">
                                 <input name="name" type="text" class="form-control" placeholder="Enter Project Name">
                               </div>
                               <div class="form-group">
-                                <input name="logo" type="file" class="form-control" placeholder="Select Project Logo ">
+                                <input name="logo" type="file" class="form-control" accept="image/png, image/svg, image/jpeg, image/jpg" placeholder="Select Project Logo ">
                               </div>
 
                             </div>
@@ -54,9 +124,7 @@
                               <div class="form-group">
                                 <div class="form-select-list">
                                   <select class="form-control custom-select-value" name="deliverable">
-                                    <option>Select Deliverable Type</option>
-                                    <option>Select 2</option>
-                                    <option>Select 3</option>
+                                    <?php echo $options; ?>
                                   </select>
                                 </div>
                               </div>
@@ -69,7 +137,7 @@
                           <div class="row">
                             <div class="col-lg-12">
                               <div class="payment-adress">
-                                <button type="submit" class="btn btn-primary waves-effect waves-light">Submit</button>
+                                <button type="submit" class="btn btn-primary waves-effect waves-light" name="submit_project_create" value="submit_project_create">Submit</button>
                               </div>
                             </div>
                           </div>
