4fe94372ca33d41fec20d8bcf6a67dbc45f41f3c
Pritesh Yadav <priteshyadav2015@gmail.com>
2023-04-19 19:03:06 +0530

    uuid added for file name

--- dashboard/library/validation/Validators/FileValidator.php
+++ dashboard/library/validation/Validators/FileValidator.php
@@ -105,12 +105,15 @@ class FileValidator
     /**
      * It upload a file which was on instance 
      *
-     * @return void
+     * @return string|boolean
      */
-    public function upload()
+    public function upload($filename = null)
     {
         $tempFile = $this->getTempFile();
-        $targetFile = $this->_targetDir . "/" . $this->getTargetFile();
+        if (!$filename) {
+            $filename = $this->guid();
+        }
+        $targetFile = $this->_targetDir . "/" . $this->getTargetFile($filename);
 
         if (!file_exists($this->_targetDir)) {
             mkdir($this->_targetDir);
@@ -120,7 +123,38 @@ class FileValidator
             return false;
         }
 
-        return true;
+        return  $targetFile;
+    }
+
+
+    public function guid()
+    {
+        $randomString = openssl_random_pseudo_bytes(16);
+        $time_low = bin2hex(substr($randomString, 0, 4));
+        $time_mid = bin2hex(substr($randomString, 4, 2));
+        $time_hi_and_version = bin2hex(substr($randomString, 6, 2));
+        $clock_seq_hi_and_reserved = bin2hex(substr($randomString, 8, 2));
+        $node = bin2hex(substr($randomString, 10, 6));
+
+        /**
+         * Set the four most significant bits (bits 12 through 15) of the
+         * time_hi_and_version field to the 4-bit version number from
+         * Section 4.1.3.
+         * @see http://tools.ietf.org/html/rfc4122#section-4.1.3
+         */
+        $time_hi_and_version = hexdec($time_hi_and_version);
+        $time_hi_and_version = $time_hi_and_version >> 4;
+        $time_hi_and_version = $time_hi_and_version | 0x4000;
+
+        /**
+         * Set the two most significant bits (bits 6 and 7) of the
+         * clock_seq_hi_and_reserved to zero and one, respectively.
+         */
+        $clock_seq_hi_and_reserved = hexdec($clock_seq_hi_and_reserved);
+        $clock_seq_hi_and_reserved = $clock_seq_hi_and_reserved >> 2;
+        $clock_seq_hi_and_reserved = $clock_seq_hi_and_reserved | 0x8000;
+
+        return sprintf('%08s-%04s-%04x-%04x-%012s', $time_low, $time_mid, $time_hi_and_version, $clock_seq_hi_and_reserved, $node);
     }
     /**
      * Return file size in KB
@@ -152,10 +186,14 @@ class FileValidator
      *
      * @return string
      */
-    public  function getTargetFile(): string
+    public  function getTargetFile($filename): string
     {
         if ($this->isFileUploaded()) {
-            $targetFile = basename($_FILES[$this->_key]['name']);
+            if (!$filename) {
+                $targetFile = basename($_FILES[$this->_key]['name']);
+            } else {
+                $targetFile = $filename . "." . pathinfo($_FILES[$this->_key]['name'], PATHINFO_EXTENSION);
+            }
             return $targetFile;
         }
         return "/";
